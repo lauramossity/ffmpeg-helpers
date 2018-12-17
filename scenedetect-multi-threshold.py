4f546e56969d293ec1e0440df57519ee0147e606
Laura Moss <lauramossity@gmail.com>
2018-12-17 17:26:02 -0500

    Scene detect script: move output to unique subfolder, add file output for timecodes

--- scenedetect-multi-threshold.py
+++ scenedetect-multi-threshold.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import os
 import argparse
+from io import StringIO
 
 import scenedetect
 from scenedetect.video_manager import VideoManager
@@ -11,7 +12,8 @@ from scenedetect.detectors import ContentDetector
 
 def main(source_path):
     SOURCE_FILENAME = os.path.splitext(os.path.basename(source_path))[0]
-    STATS_FILE_PATH = os.path.basename(source_path) + '.stats.csv'
+    OUTPUT_FOLDER = os.path.join(os.getcwd(), SOURCE_FILENAME)
+    STATS_FILE_PATH = os.path.join(OUTPUT_FOLDER, SOURCE_FILENAME + '.stats.csv')
 
     HIGH_CONTENT_THRESHOLD = 55.0 # Try to get big scene changes - new scenery/setting
     LOW_CONTENT_THRESHOLD = 40.0 # Try to pick up smaller scene changes like camera cuts in same location/setting
@@ -63,12 +65,20 @@ def main(source_path):
             with open(STATS_FILE_PATH, 'w') as stats_file:
                 stats_manager.save_to_csv(stats_file, base_timecode)
 
-        print('List of scenes obtained, high threshold (%s):' % HIGH_CONTENT_THRESHOLD)
+        print(';'.join(start.get_timecode() + ',' + end.get_timecode() for start, end in scene_list_high_threshold), file=open(os.path.join(OUTPUT_FOLDER, SOURCE_FILENAME + '-majorScenes.txt'), 'w'))
+        print(','.join(frametimecode.get_timecode() for frametimecode in scene_manager.get_cut_list(base_timecode)), file=open(os.path.join(OUTPUT_FOLDER, SOURCE_FILENAME + '-majorSceneCuts.txt'), 'w'))
+        
+        buf = StringIO()
+        buf.write('List of scenes obtained, high threshold (%s):\n' % HIGH_CONTENT_THRESHOLD)
         for i, scene in enumerate(scene_list_high_threshold):
-            print('    Scene %2d: Start,End Timecodes: %s,%s Frames: %d,%d' % (
+            buf.write('    Scene %2d: Start,End Timecodes: %s,%s Frames: %d,%d\n' % (
                 i+1,
                 scene[0].get_timecode(), scene[1].get_timecode(),
                 scene[0].get_frames(), scene[1].get_frames()))
+
+        # Print detailed list to both stdout and file.
+        print(buf.getvalue())
+        print(buf.getvalue(), file=open(os.path.join(OUTPUT_FOLDER, SOURCE_FILENAME + '-majorScenesPrettyPrint.txt'), 'w'))
 
         # scene_manager.write_scene_list(os.path.basename(source_path) + '.scenes.csv', scene_list_high_threshold)
 
@@ -81,13 +91,21 @@ def main(source_path):
 
         scene_manager.detect_scenes(frame_source=video_manager)
         scene_list_low_threshold = scene_manager.get_scene_list(base_timecode)
+        
+        print(';'.join(start.get_timecode() + ',' + end.get_timecode() for start, end in scene_list_low_threshold), file=open(os.path.join(OUTPUT_FOLDER, SOURCE_FILENAME + '-minorScenes.txt'), 'w'))
+        print(','.join(frametimecode.get_timecode() for frametimecode in scene_manager.get_cut_list(base_timecode)), file=open(os.path.join(OUTPUT_FOLDER, SOURCE_FILENAME + '-minorSceneCuts.txt'), 'w'))
 
-        print('List of scenes obtained, low threshold (%s):' % LOW_CONTENT_THRESHOLD)
+        buf = StringIO()
+        buf.write('List of scenes obtained, low threshold (%s):\n' % LOW_CONTENT_THRESHOLD)
         for i, scene in enumerate(scene_list_low_threshold):
-            print('    Scene %2d: Start,End Timecodes: %s,%s Frames: %d,%d' % (
+            buf.write('    Scene %2d: Start,End Timecodes: %s,%s Frames: %d,%d\n' % (
                 i+1,
                 scene[0].get_timecode(), scene[1].get_timecode(),
                 scene[0].get_frames(), scene[1].get_frames()))
+        
+        # Print detailed list to both stdout and file.
+        print(buf.getvalue())
+        print(buf.getvalue(), file=open(os.path.join(OUTPUT_FOLDER, SOURCE_FILENAME + '-minorScenesPrettyPrint.txt'), 'w'))
 
         # TODO save timecodes to csv or text file
 
@@ -95,14 +113,13 @@ def main(source_path):
         if scenedetect.video_splitter.is_ffmpeg_available():
             if scene_list_high_threshold:
                 scenedetect.video_splitter.split_video_ffmpeg(video_manager.get_video_paths(), scene_list_high_threshold,
-                    os.path.join(os.getcwd(), '${VIDEO_NAME}-major-scene-${SCENE_NUMBER}.mp4'), SOURCE_FILENAME, 
-                    arg_override='-c:v libx264 -preset ultrafast -vf scale=320:-2 -crf 32 -c:a copy')
-
+                    os.path.join(OUTPUT_FOLDER, '${VIDEO_NAME}-major-scene-${SCENE_NUMBER}.mp4'), SOURCE_FILENAME, 
+                    arg_override='-c:v libx264 -preset ultrafast -vf scale=320:-2 -crf 32')
 
             if scene_list_low_threshold:
                 scenedetect.video_splitter.split_video_ffmpeg(video_manager.get_video_paths(), scene_list_low_threshold, 
-                    os.path.join(os.getcwd(), '${VIDEO_NAME}-minor-scene-${SCENE_NUMBER}.mp4'), SOURCE_FILENAME,
-                    arg_override='-c:v libx264 -preset ultrafast -vf scale=320:-2 -crf 32 -c:a copy')
+                    os.path.join(OUTPUT_FOLDER, '${VIDEO_NAME}-minor-scene-${SCENE_NUMBER}.mp4'), SOURCE_FILENAME,
+                    arg_override='-c:v libx264 -preset ultrafast -vf scale=320:-2 -crf 32')
 
     finally:
         video_manager.release()
