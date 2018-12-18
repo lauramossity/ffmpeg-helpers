56b195af7fde1e15b586f276bb39384ba1818bd3
Laura Moss <lauramossity@gmail.com>
2018-12-17 23:14:15 -0500

    export-video script points to filter script and preset files by default

--- export-video.ps1
+++ export-video.ps1
@@ -21,14 +21,22 @@ if($timestamps) {
 #Find interlaced frames, deinterlace only interlaced frames, denoise, remove 8px from left and bottom, add black padding back, ensure original scale
 $videoFilters = "yadif=mode=send_field:parity=tff:deint=all, hqdn3d=8, crop=632:472:8:0, scale=640:480"
 
+$defaultFilterScriptLoc = Join-Path -Path $PSScriptRoot -ChildPath "deinterlace-hq.filter"
+
 if($vfilterscript) {
 	$videoFilterArgs = "-filter_script:v $vfilterscript"
+} elseif(Test-Path $defaultFilterScriptLoc) {
+	$videoFilterArgs = "-filter_script:v $defaultFilterScriptLoc"
 } else {
 	$videoFilterArgs = "-vf '$videoFilters'"
 }
 
+$defaultPresetFileLoc = Join-Path -Path $PSScriptRoot -ChildPath "lm-camcorder-hq.ffpreset"
+
 if($vpresetfile) {
 	$videoPresetArgs = "-fpre:v $vpresetfile"
+} elseif(Test-Path $defaultPresetFileLoc) {
+	$videoPresetArgs = "-fpre:v $defaultPresetFileLoc"
 }
 
 # Audio: 192kbps, 48000, Select left audio channel to mono
