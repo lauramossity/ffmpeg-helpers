b301dc24528fef13342f8337fbed2448d0a6a68e
Laura Moss <lauramossity@gmail.com>
2019-07-16 20:28:02 -0400

    Generify main script by using subparsers in arg parsing

--- ffmpeg-helpers.py
+++ ffmpeg-helpers.py
@@ -38,32 +38,56 @@ def export_video(args):
     print(' '.join(ffmpegArgs) + '\n\n')
     subprocess.run(ffmpegArgs)
 
-if __name__ == "__main__":
+def create_gif(args):
+    print("Create Gif function")
+
+def edit_video(args):
+    print("Edit Video function")
+
+def init_video_clip_subparser(subparsers, name, function, helpMessage, defaultFilterScript, defaultPresetfile):
     SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-    DEFAULT_PRESET_FILE = "lm-camcorder-hq.ffpreset"
-    DEFAULT_FILTER_SCRIPT = "deinterlace-hq.filter"
-    
-    # TODO use subparsers
 
-    parser = argparse.ArgumentParser()
+    subparser = subparsers.add_parser(name, help=helpMessage)
+    subparser.set_defaults(func=function)
 
-    ioGroup = parser.add_argument_group("I/O")
+    # TODO make destination optional
+    ioGroup = subparser.add_argument_group("I/O")
     ioGroup.add_argument("-s", "--source", help="path to the source video", required=True)
     ioGroup.add_argument("-d", "--destination", metavar="DEST", help="path to the destination video file", required=True)
     
-    videoSettingsGroup = parser.add_argument_group("ffmpeg video settings")
+    videoSettingsGroup = subparser.add_argument_group("ffmpeg video settings")
     videoSettingsGroup.add_argument("--vfs", "--vfilterscript", metavar="VIDEO_FILTER_SCRIPT",
-                        help="ffmpeg filter script to be used in the -filter_script:v argument (default: " + DEFAULT_FILTER_SCRIPT + ")", 
-                        default=os.path.join(SCRIPT_DIR, DEFAULT_FILTER_SCRIPT))
+                        help="ffmpeg filter script to be used in the -filter_script:v argument (default: " + defaultFilterScript + ")", 
+                        default=os.path.join(SCRIPT_DIR, defaultFilterScript))
     videoSettingsGroup.add_argument("--vpf", "--vpresetfile", metavar="VIDEO_PRESET_FILE",
-                        help="ffmpeg preset file to be used in the -fpre:v argument (default: " + DEFAULT_PRESET_FILE + ")",
-                        default=os.path.join(SCRIPT_DIR, DEFAULT_PRESET_FILE))
+                        help="ffmpeg preset file to be used in the -fpre:v argument (default: " + defaultPresetfile + ")",
+                        default=os.path.join(SCRIPT_DIR, defaultPresetfile))
     
+    # TODO make segment mandatory for gif
     # https://trac.ffmpeg.org/wiki/Seeking
-    timestampGroup = parser.add_argument_group("Extract segment", "See https://trac.ffmpeg.org/wiki/Seeking for details")
+    timestampGroup = subparser.add_argument_group("Extract segment", "See https://trac.ffmpeg.org/wiki/Seeking for details")
     timestampMutexGroup = timestampGroup.add_mutually_exclusive_group()
     timestampMutexGroup.add_argument("--time-segment-fast", nargs=2, metavar=("START_TIMESTAMP", "TIME_LENGTH"), help="Time segment to extract using the keyframe method. May be inaccurate on stream copy. Example: 1:01.123 2")
     timestampMutexGroup.add_argument("--time-segment-slow", nargs=2, metavar=("START_TIMESTAMP", "END_TIMESTAMP"), help="Time segment to extract using a slow seek. Example: 1:01.123 1:03:123")
     
+    return subparser
+
+if __name__ == "__main__":
+    DEFAULT_FILTER_SCRIPT = "deinterlace-hq.filter"
+    DEFAULT_PRESET_FILE = "lm-camcorder-hq.ffpreset"
+
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(title='subcommands')
+
+    init_video_clip_subparser(subparsers, 'export', export_video, "Convert a video or video segment to a new format", DEFAULT_FILTER_SCRIPT, DEFAULT_PRESET_FILE)
+
+    # TODO destination should be gif location? or just autogenerate file name
+    init_video_clip_subparser(subparsers, 'gif', create_gif, "Export a video clip as a gif", "deinterlace-small.filter", "lm-camcorder-small.ffpreset")
+
+    edit_parser = subparsers.add_parser("edit", help="Remove segments from a video")
+    edit_parser.set_defaults(func=edit_video)
+    edit_parser.add_argument("-s", "--source", help="path to the source video", required=True)
+    edit_parser.add_argument("--segmentsToCut", help="path to a CSV file of segments to cut", required=True)
+
     args = parser.parse_args()
-    export_video(args)
+    args.func(args)
